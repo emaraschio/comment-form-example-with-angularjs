187237373e8bc00687b935c70a9b2aed284ace0a
Ezequiel Maraschio <ezequiel.maraschio@gmail.com>
2014-09-19 17:32:05 -0300

    List comments feauture

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 (function() {
   var app = angular.module('commentForm', []);
 
-  app.controller('MainController', function() {
+  app.controller('ListController', function() {
+    this.list = comments;
+  });
+
+  app.controller('CommentController', function() {
     this.comment = {};
-    
-    this.addComment = function(comment) {
-      this.comment = comment;
+
+    this.addComment = function(list) {
+      list.push(this.comment);
+      this.comment = {};
     };
   });
 
- 
+  var comments = [
+    {
+      body: 'Example Comment 1', author: 'test@test.com'
+    },
+    {
+      body: 'Example Comment 2', author: 'test@test.org'
+    }
+  ];
 })();
-
